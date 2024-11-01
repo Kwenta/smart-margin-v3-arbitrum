ecb3e5ab3561be493d012021afff66a29dc57d8d
Flocqst <flocastet@icloud.com>
2024-11-01 15:26:50 +0100

    🚀 arb zap odos deploy

--- script/utils/parameters/ArbitrumParameters.sol
+++ script/utils/parameters/ArbitrumParameters.sol
@@ -17,7 +17,7 @@ contract ArbitrumParameters {
 
     address public constant WETH = 0x82aF49447D8a07e3bd95BD0d56f35241523fBab1;
 
-    address public constant ZAP = 0xD1F129e0cDd3Cc5c65ea00041623841C3d709F83;
+    address public constant ZAP = 0x5225dFdC934f9569d2612a082E1F66B16DD5d624;
 
     address public constant USDT = 0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9;
 
